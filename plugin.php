4c13cdb542d4dd8558403983be5b0f8771b63248
smeyer <smeyer.ilias@gmx.de>
2020-03-12 10:32:20 +0100

    Increased version

--- plugin.php
+++ plugin.php
@@ -26,12 +26,12 @@
 $id = "authmssso";
  
 // code version; must be changed for all code changes
-$version = "5.3.8.1";
+$version = "5.4.1.1";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.3.8";
-$ilias_max_version = "5.3.99";
+$ilias_min_version = "5.4.5";
+$ilias_max_version = "5.4.99";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
