648f66f1500cdc562a8b593e24d0a787a25e3b02
smeyer <smeyer.ilias@gmx.de>
2022-11-18 17:58:07 +0100

    increased version

--- plugin.php
+++ plugin.php
@@ -1,39 +1,33 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
  
 // alphanumerical ID of the plugin; never change this
 $id = "authmssso";
  
 // code version; must be changed for all code changes
-$version = "5.4.1.1";
+$version = "7.10.1";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.5";
-$ilias_max_version = "5.4.99";
+$ilias_min_version = "7.10";
+$ilias_max_version = "7.99";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
-$responsible_mail = "meyer@leifos.com";
+$responsible_mail = "meyer@leifos.de";
 ?>
